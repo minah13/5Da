0afacffdfd872920ba480b53f9018d6b8d9783dc
minah13 <hdmksm1@naver.com>
2023-02-06 12:22:37 +0900

    Project Configuration Setting

---
+++ 5Da/src/main/java/spring/project/da/Application.java
@@ -0,0 +1,13 @@
+package spring.project.da;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ 5Da/src/test/java/spring/project/da/ApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.project.da;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
